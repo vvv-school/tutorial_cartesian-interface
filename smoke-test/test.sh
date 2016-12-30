a91e80dde653d1bf98bf42e45252311ca6015477
Ugo Pattacini <ugo.pattacini@iit.it>
2016-12-30 16:32:48 +0100

    update

--- smoke-test/test.sh
+++ smoke-test/test.sh
@@ -4,44 +4,47 @@
 # Authors: Ugo Pattacini <ugo.pattacini@iit.it>
 # CopyPolicy: Released under the terms of the GNU GPL v3.0.
 
+echo "Installing the tutorial"
 rm build -rf
 mkdir build && cd build
-cmake ../../
-make install && cd ../
+cmake -DCMAKE_BUILD_TYPE=Release ../../ > /dev/null 2>&1
+make install > /dev/null 2>&1
+cd ../
 
+echo "Compiling the test"
 rm build-test -rf
 mkdir build-test && cd build-test
-cmake -DCMAKE_BUILD_TYPE=Release ../
-make && cd ../
+cmake -DCMAKE_BUILD_TYPE=Release ../ > /dev/null 2>&1
+make > /dev/null 2>&1
+cd ../
 
 # to let yarpmanager access the fixture
-if [ -z "$YARP_DATA_DIRS" ]; then
-	export YARP_DATA_DIRS=$(pwd)
-else
-	export YARP_DATA_DIRS=${YARP_DATA_DIRS}:$(pwd)
-fi
+export YARP_DATA_DIRS=${YARP_DATA_DIRS}:$(pwd)
 
 # to make the test library retrievable
-if [ -z "$LD_LIBRARY_PATH" ]; then
-	export LD_LIBRARY_PATH=$(pwd)/build-test/plugins
-else
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/build-test/plugins
-fi
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(pwd)/build-test/plugins
 
-if ! yarp where
+if ! yarp where > /dev/null 2>&1
 then
    kill_yarp="yes"
-   yarpserver --write &
+   yarpserver --write > /dev/null 2>&1 &
    sleep 1
 else
    kill_yarp="no"
 fi
 
 testrunner --verbose --suit test.xml
-
-cd build
-make uninstall && cd ../
+if [ $? -eq 0 ]; then 
+   echo "TEST PASSED!"
+else
+   echo "TEST FAILED!"
+fi
 
 if [ "$kill_yarp" == "yes" ]; then
-   killall yarpserver
+   killall yarpserver > /dev/null 2>&1
 fi
+
+echo "Uninstalling the tutorial"
+cd build
+make uninstall > /dev/null 2>&1
+cd ../
